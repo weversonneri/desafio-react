c4dfcd71d69612106b300450d53ce4d1315a5b20
weversonneri <weversonneri@outlook.com>
2021-06-27 17:37:45 -0300

    :sparkles: create remove item function
    create function to remove single item and function to remove all recurrence of product

--- src/context/cart.js
+++ src/context/cart.js
@@ -4,17 +4,45 @@ const CartContext = createContext();
 
 function CartProvider({ children }) {
   const [cart, setCart] = useState([]);
+
   const cartTotalPrice = cart.reduce((accumPrice, { price }) => accumPrice + price, 0);
+
   const addToCart = (product) => setCart((currentCart) => [...currentCart, product]);
 
   const cartQuantityOfItem = (id) => cart.filter((product) => product.id === id).length;
+
+  const removeSigleFromCart = (product) => {
+    setCart((currentCart) => {
+      const indexOfItemToRemove = currentCart.findIndex((cartItem) => cartItem.id === product.id);
+
+      if (indexOfItemToRemove === -1) {
+        return currentCart;
+      }
+
+      return [
+        ...currentCart.slice(0, indexOfItemToRemove),
+        ...currentCart.slice(indexOfItemToRemove + 1),
+      ];
+    });
+  };
+
+  const removeItemFromCart = (product) => {
+    const newCart = cart.filter((cartItem) => cartItem.id !== product.id);
+    setCart(newCart);
+  };
+
   const uniqueProduct = (product) => Array.from(new Set(product));
+
   return (
+    <CartContext.Provider value={{
       cart,
       addToCart,
       cartQuantityOfItem,
       cartTotalPrice,
+      removeItemFromCart,
+      removeSigleFromCart,
       uniqueProduct
+    }}>
       {children}
     </CartContext.Provider>
   );
