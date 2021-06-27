18fca65810d0683d59644b1aafdc5040ff7cf3f5
weversonneri <weversonneri@outlook.com>
2021-06-27 08:10:24 -0300

    :sparkles: create product card list

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,19 +1,46 @@
-import React from 'react'
-import { useHistory } from 'react-router-dom';
+import React from "react";
+import { useCart } from '../../context/cart';
+
+import images from '../../assets/call-of-duty-infinite-warfare.png';
+
+import './styles.css';
+
+import products from '../../services/products.json';
+import { NavBar } from "../../components/NavBar";
 
 export function Home() {
-  const history = useHistory();
-
-  function handleClick() {
-    history.push('/checkout');
-  }
 
   return (
-    <div>
-      <h1>Home</h1>
-      <button type="button" onClick={handleClick}>
-        checkout
-    </button>
-    </div>
-  )
+    <>
+      <NavBar />
+      <div className="container">
+
+        <div className="cards">
+          {products.map((product) => (
+
+            <div className="card"
+              key={product.id}
+            >
+              <img src={images} alt="Produto" className="card-image" />
+              <div className="card-content">
+                <h5 className="card-title">
+                  {product.name}
+                </h5>
+                <h3 className="card-price">
+                  R$ {product.price}
+                </h3>
+              </div>
+              <button
+                className="card-button"
+                onClick={() => console.log(product.id)} >
+                Comprar
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
+
+
