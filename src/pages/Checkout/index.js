9b424cfb2599b4e8820e00f0ac5dd97a834f73ab
weversonneri <weversonneri@outlook.com>
2021-06-27 17:40:20 -0300

    :sparkles: display product in cart

--- src/pages/Checkout/index.js
+++ src/pages/Checkout/index.js
@@ -1,11 +1,37 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { NavBar } from "../../components/NavBar";
+import { Footer } from "../../components/Footer";
+
+import './styles.css';
+
+import images from '../../assets/call-of-duty-infinite-warfare.png';
+import { CheckoutProductsCard } from '../../components/CheckoutProductsCard';
+import { useCart } from '../../context/cart';
 
 export function Checkout() {
+  const { cart, uniqueProduct } = useCart();
+
   return (
-    <div>
-      <h1>checkout</h1>
-      <Link to="/">Continuar comprando</Link>
-    </div>
+    <div className="checkout-page-container">
+      <NavBar />
+      <div className="checkout-content">
+        <h1>Meu Carrinho</h1>
+        <Link to="/">Continuar comprando</Link>
+        <div className="checkout-card-wrapper">
+          {uniqueProduct(cart).map((product) => (
+            <CheckoutProductsCard
+              key={product.id}
+              image={images}
+              {...product}
+            />
+          ))}
+        </div>
+        <div className="summary-card">
+        </div>
+      </div>
+      <Footer />
+    </div >
   )
 }
