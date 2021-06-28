ce63d6c8943a04f9a5f5cb4a71b468dacabd63ca
weversonneri <weversonneri@outlook.com>
2021-06-28 18:56:31 -0300

    :sparkles: create product sort function
    function to ordenate products

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,40 +1,74 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useCart } from '../../context/cart';
 
-import images from '../../assets/call-of-duty-infinite-warfare.png';
+import { formatCurrency } from '../../utils';
 
 import './styles.css';
 
-import products from '../../services/products.json';
+import products from '../../database/products.json';
 import { NavBar } from "../../components/NavBar";
 import { Footer } from "../../components/Footer";
 
 export function Home() {
+  const [data, setData] = useState([]);
+  const [sortType, setSortType] = useState('name');
   const { addToCart } = useCart();
+
+  function handleAddToCart(product) {
+    addToCart(product);
+    alert('Jogo adicionado ao carrinho')
+  }
+
+  useEffect(() => {
+    const sortProducts = (type) => {
+      const sorted = [...products].sort((a, b) => {
+        if (type === 'score') {
+          return b[type].toString().localeCompare(a[type].toString(), 'pt-BR', { numeric: true })
+        }
+        return a[type].toString().localeCompare(b[type].toString(), 'pt-BR', { numeric: true })
+      });
+      setData(sorted);
+    }
+    sortProducts(sortType);
+  }, [sortType]);
 
   return (
     <>
       <NavBar />
       <div className="container">
-
+        <div className="products-header">
+          <h2>Jogos</h2>
+          <div className="sort-list-container">
+            <p>Ordenar por</p>
+            <select
+              value={sortType}
+              className="sort-list"
+              onChange={(e) => setSortType(e.target.value)}
+            >
+              <option value="price">Preço</option>
+              <option value="score">Popularidade</option>
+              <option value="name">Ordem Alfabética</option>
+            </select>
+          </div>
+        </div>
         <div className="cards">
-          {products.map((product) => (
+          {data.map((product) => (
 
             <div className="card"
               key={product.id}
             >
-              <img src={images} alt="Produto" className="card-image" />
+              <img src={require(`../../assets/${product.image}`).default} alt="Produto" className="card-image" />
               <div className="card-content">
                 <h5 className="card-title">
                   {product.name}
                 </h5>
                 <h3 className="card-price">
-                  R$ {product.price}
+                  {formatCurrency(product.price)}
                 </h3>
               </div>
               <button
                 className="card-button"
-                onClick={() => addToCart(product)} >
+                onClick={() => handleAddToCart(product)} >
                 Comprar
               </button>
             </div>
